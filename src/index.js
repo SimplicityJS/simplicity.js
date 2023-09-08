7acf26ccac28a19ebc1d093e5392750893203b0e
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2023-09-08 14:28:05 -0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const SimplicityClient = require("./classes/SimplicityClient");
+const Util = require("./classes/Util.js");
+
+/**
+ * SimplicityClient, custom events, command loader, client shard, error handling and utility functions
+ * @namespace
+ */
+module.exports = {
+  SimplicityClient,
+  Util
+};
